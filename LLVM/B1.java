bd7d999cfd929a43f112d04a2fdf940e066a02d6
Arjun Bharat <arjunbharat99@gmail.com>
2022-10-05 13:40:30 +0530

    Updated some source files and removed unnecessary ones

--- LLVM/B1.java
+++ LLVM/B1.java
@@ -1,5 +1,6 @@
 import syntaxtree.*;
 import visitor.*;
+import java.util.*;
 public class B1
 {
 	public static void main(String[] arg)
@@ -7,11 +8,13 @@ public class B1
 		try
 		{
 			Node root = new MiniJavaParser(System.in).Goal();
-			Integer x = 0;
-			GJVoidDepthFirst<Integer> obj = new GJVoidDepthFirst<Integer>();
+			Integer x = new Integer(0);
+			myDFS obj = new myDFS<String,Integer>();
+       		root.accept(obj,x); // Your assignment part is invoked here.
+            //System.out.println("First pass successful!\n");
+			x = new Integer(1);
 			root.accept(obj,x);
-			x++;   
-			root.accept(obj,x);
+			//System.out.println("Program type checked successfully");
       	}
       	catch (ParseException e) {
          	System.out.println(e.toString());
